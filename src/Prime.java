c2faedc2148b6f4cf2f4ccfe9606a2a96283f5c6
Duc-khanh <nguyenkhanh2561990@gmail.com>
2026-06-02 13:59:19 +0700

    first commit

---
+++ src/Prime.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Prime {
+    public static void main(String[] args) {
+        for (int i = 1 ; i <= 100 ; i ++){
+            System.out.println(i);
+        }
+    }
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.print("Enter a number:");
+//        int number = scanner.nextInt();
+//        if (number < 2){
+//            System.out.println(number + " is not prime");
+//        } else {
+//            boolean isPrime = true;
+//            for (int i = 2; i < number; i++) {
+//                if (number % i == 0) {
+//                    isPrime = false;
+//                    break;
+//                }
+//            }
+//            if (isPrime) {
+//                System.out.println(number + " is prime");
+//            } else {
+//                System.out.println(number + " is not prime");
+//            }
+//        }
+//    }
+}
