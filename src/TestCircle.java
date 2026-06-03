b472f0e87174fe96f3da126598d981d2ee135849
Duc-khanh <nguyenkhanh2561990@gmail.com>
2026-06-03 20:37:05 +0700

    done exercise access modifier

---
+++ src/TestCircle.java
@@ -0,0 +1,10 @@
+public class TestCircle {
+    public static void main(String[] args) {
+
+        Circle circle = new Circle(5);
+
+        System.out.println("Radius: " + circle.getRadius());
+        System.out.println("Area: " + circle.getArea());
+
+    }
+}
