93bd442d77ca1b1279545521873af06103278bc2
Duc-khanh <nguyenkhanh2561990@gmail.com>
2026-06-10 16:31:34 +0700

    feature/10_06_2026_MyGenericStack

---
+++ src/MyGenericStack.java
@@ -0,0 +1,61 @@
+import java.util.EmptyStackException;
+import java.util.LinkedList;
+
+
+public class MyGenericStack<T> {
+    private LinkedList<T> stack;
+
+    public MyGenericStack() {
+        stack = new LinkedList();
+    }
+    // Thêm phần tử lên đỉnh Stack
+    public void push(T item) {
+        stack.addFirst(item);
+    }
+    // Lấy phần tử trên cùng ra khỏi Stack
+    public T pop() {
+        if (isEmpty()) {
+            throw new EmptyStackException();
+        }
+        return stack.removeFirst();
+    }
+    // tra về so ptu
+    public int size() {
+        return stack.size();
+    }
+// ktr xem stack rong hay khong
+    private boolean isEmpty() {
+        if (stack.size() == 0) {
+            return true;
+        }
+        return false;
+    }
+    static void stackOfIStrings() {
+        MyGenericStack<String> stack = new MyGenericStack();
+        stack.push("Five");
+        stack.push("Four");
+        stack.push("Three");
+        stack.push("Two");
+        stack.push("One");
+        System.out.println("1.1 Size of stack after push operations: " + stack.size());
+        System.out.printf("1.2. Pop elements from stack : ");
+        while (!stack.isEmpty()) {
+            System.out.printf(" %s", stack.pop());
+        }
+        System.out.println("\n1.3. Size of stack after pop operations : " + stack.size());
+    }
+    static void stackOfIntegers() {
+        MyGenericStack<Integer> stack = new MyGenericStack();
+        stack.push(5);
+        stack.push(4);
+        stack.push(3);
+        stack.push(2);
+        stack.push(1);
+        System.out.println("2.1. Size of stack after push operations: " + stack.size());
+        System.out.printf("2.2. Pop elements from stack : ");
+        while (!stack.isEmpty()) {
+            System.out.printf(" %d", stack.pop());
+        }
+        System.out.println("\n3.3 Size of stack after pop operations : " + stack.size());
+    }
+}
